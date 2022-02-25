7c276c0a91be2c61b1e6d076c1f661ac1697d2b3
EngineerSmith <pasmith.bus@gmail.com>
2022-02-25 23:15:57 +0000

    Base conf

---
+++ conf.lua
@@ -0,0 +1,67 @@
+local identity = "love-jam-2022-ES"
+love.filesystem.setIdentity(identity, true)
+
+local args = require("util.args")
+
+local baseConf = function(t)
+  t.identity = identity
+  t.appendidentity = true
+  t.version = "11.4"
+  t.console = true
+  t.accelerometerjoystick = false
+  
+  t.modules.sound = false
+  t.modules.touch = false
+  t.modules.video = false
+  t.modules.physics = false
+end
+
+if args["-server"] then
+  love.conf = function(t)
+    baseConf(t)
+    t.window.title = "Sever"
+    
+    t.modules.data = true
+    t.modules.event = true
+    t.modules.math = true
+    t.modules.system = true
+    t.modules.thread = true
+    t.modules.timer = true
+    
+    t.modules.audio = false
+    t.modules.font = false
+    t.modules.graphics = false
+    t.modules.image = false
+    t.modules.joystick = false
+    t.modules.keyboard = false
+    t.modules.mouse = false
+    t.modules.window = false
+  end
+else
+  love.conf = function(t)
+    baseConf(t)
+    t.gammacorrect = true
+    
+    t.window.title = "Client"
+    t.window.icon = nil
+    t.window.resizable = true
+    t.window.display = 1
+    t.window.highdpi = true
+    t.window.depth = 24
+    
+    t.modules.audio = true
+    t.modules.data = true
+    t.modules.event = true
+    t.modules.font = true
+    t.modules.graphics = true
+    t.modules.image = true
+    t.modules.joystick = true
+    t.modules.keyboard = true
+    t.modules.math = true
+    t.modules.mouse = true
+    t.modules.system = true
+    t.modules.thread = true
+    t.modules.timer = true
+    t.modules.window = true
+  end
+end
