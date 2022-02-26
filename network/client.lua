0a11c8620d1d8265a7c81a34f1448ded8ca3e03f
EngineerSmith <pasmith.bus@gmail.com>
2022-02-26 01:49:43 +0000

    Added basic functionality for client

--- network/client.lua
+++ network/client.lua
@@ -5,6 +5,8 @@ local serialize = require("network.serialize")
 local enum = require("network.enum")
 local enumPT = enum.packetType
 
+local remove, insert = table.remove, table.insert
+
 local client = {
     enum = enumPT,
     handlers = { },
@@ -25,6 +27,48 @@ client.connect = function(address, login)
   cmdIn:push(login)
   client.thread:start(address)
   client.address = address
+end
+
+client.handle = function(packetType, encoded)
+  local decoded
+  if encoded then
+    decoded = serialize.decode(encoded)
+  end
+  if packetType == enumPT.receive then
+    local pt = decoded[1]
+    remove(decoded, 1)
+    for _, callback in ipairs(client.handlers[pt]) do
+      callback(unpack(decoded))
+    end
+  elseif packetType == enumPT.disconnect then
+    logger.info("Disconnected, reason:", decoded[1], "code:", decoded[2])
+    client.isConnected = false
+    for _, callback in ipairs(client.handlers[enumPT.disconnect]) do
+      callback(unpack(decoded))
+    end
+  elseif packetType == enumPT.confirmConnection then
+    logger.info("Successful connection made")
+    client.isConnected = true
+    for _, callback in ipairs(client.handlers[enumPT.confirmConnection]) do
+      callback(unpack(decoded))
+    end
+  end
+end
+
+client.addHandler = function(packetType, callback)
+  insert(client.handlers[packetType], callback)
+end
+
+client.send = function(packetType, ...)
+  local encoded = serialize.encode(packetType, ...)
+  if encoded then
+    cmdIn:push(encoded)
+  end
+end
+
+client.disconnect = function(reason)
+  reason = reason or enum.disconnect.normal
+  cmdIn:push(enumPT.disconnect..reason)
 end
 
 return client
