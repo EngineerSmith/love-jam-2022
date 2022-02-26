84f364bb535103445e7cf1f3a3e04981ee309fb8
EngineerSmith <pasmith.bus@gmail.com>
2022-02-26 00:44:50 +0000

    logging

--- server.lua
+++ server.lua
@@ -1,6 +1,13 @@
+local logger = require("util.logger")
+
 local scene = { }
 
+local loader = require("util.serverLoader")
+
 scene.load = function(port)
+  logger.info("Server loading assets")
+  loader()
+  logger.info("Switching to main loop")
   require("util.sceneManager").changeScene("server.main")
 end
 
