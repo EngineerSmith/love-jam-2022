61081917f07b5099619b711d8897867a7d89d995
EngineerSmith <pasmith.bus@gmail.com>
2022-02-26 01:57:35 +0000

    Added more handerling of the thread

--- network/client.lua
+++ network/client.lua
@@ -20,13 +20,39 @@ end
 local cmdIn = lt.getChanne("cmdIn")
 
 client.connect = function(address, login)
+  if client.isConnected or not (client.thread and client.thread:isRunning()) then
+    error("Cannot connect when already connected")
+  end
   logger.info("Starting network thread, connecting to", address)
-  client.thread = lt.newThread("network/clientthread.lua")
+  if not client.thread then
+    client.thread = lt.newThread("network/clientthread.lua")
+  end
   love.handlers["cmdOut"] = client.handle
   cmdIn:clear()
   cmdIn:push(login)
   client.thread:start(address)
   client.address = address
+end
+
+client.threaderror = function(thread, errorMessage)
+  if thread == client.thread then
+    logger.warn("Error on network thread:", errorMessage)
+    client.thread = nil
+    client.isConnected = false
+    return true
+  end
+  return false
+end
+
+client.quit = function()
+  if client.thread and client.thread:isRunning() then
+    cmdIn:performAtomic(function()
+        cmdIn:clear()
+        cmdIn:push("quit")
+      end)
+    client.thread:wait()
+  end
+  client.isConnected = false
 end
 
 client.handle = function(packetType, encoded)
@@ -42,10 +68,10 @@ client.handle = function(packetType, encoded)
     end
   elseif packetType == enumPT.disconnect then
     logger.info("Disconnected, reason:", decoded[1], "code:", decoded[2])
-    client.isConnected = false
     for _, callback in ipairs(client.handlers[enumPT.disconnect]) do
       callback(unpack(decoded))
     end
+    client.quit()
   elseif packetType == enumPT.confirmConnection then
     logger.info("Successful connection made")
     client.isConnected = true
