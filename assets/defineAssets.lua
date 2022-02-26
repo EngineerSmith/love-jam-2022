283a29dbcdebad0596d401b6d3eb40ff7c6945e2
EngineerSmith <pasmith.bus@gmail.com>
2022-02-26 15:29:37 +0000

    Fixed asset formatting

--- assets/defineAssets.lua
+++ assets/defineAssets.lua
@@ -3,9 +3,9 @@ local pixelArt = function(image)
 end
 
 return { 
-    { "UI/logoES.png", "logo.ES", onLoad = pixelArt }
+    { "UI/logoES.png", "logo.ES", onLoad = pixelArt },
     
-	{ "tiles/test1.png", "tiles.test1", onLoad = pixelArt }
-    { "tiles/test2.png", "tiles.test2", onLoad = pixelArt }
-    { "tiles/test3.png", "tiles.test3", onLoad = pixelArt }
+	  { "tiles/test1.png", "tiles.test1", onLoad = pixelArt },
+    { "tiles/test2.png", "tiles.test2", onLoad = pixelArt },
+    { "tiles/test3.png", "tiles.test3", onLoad = pixelArt },
   }
