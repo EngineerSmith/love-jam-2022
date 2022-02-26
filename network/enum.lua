4681e14d4f118b1c8036ccd45afc0b7c8488fb2f
EngineerSmith <pasmith.bus@gmail.com>
2022-02-26 01:24:48 +0000

    Added basic enums that I think I will need

---
+++ network/enum.lua
@@ -0,0 +1,29 @@
+local enum = {
+    disconnect = {
+        normal = 0,
+        badconnect = 0,
+        badserver = 0,
+      },
+    packetType = { -- must be 1 char in size
+        receive           = 'r',
+        disconnect        = 'd',
+        confirmConnection = 'c',
+        -- custom
+        chatMessage       = 'm',
+        -- world data
+        playerPosition    = 'p',
+      },
+  }
+  
+enum.convert = function(value, enumType)
+  local enums = enum[enumType]
+  if not enums then return nil end
+  for k, v in pairs(enums) do
+    if v == value then
+      return k
+    end
+  end
+  return nil
+end
+
+return nil
