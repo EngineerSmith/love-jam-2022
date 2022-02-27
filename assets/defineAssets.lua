2153be73422361afecea8c16147c7a74087e77cf
Yolwoocle <leo.bernard2015@hotmail.com>
2022-02-27 16:54:06 +0100

    Duck sprite

--- assets/defineAssets.lua
+++ assets/defineAssets.lua
@@ -8,4 +8,6 @@ return {
 	  { "tiles/test1.png", "tiles.test1", onLoad = pixelArt },
     { "tiles/test2.png", "tiles.test2", onLoad = pixelArt },
     { "tiles/test3.png", "tiles.test3", onLoad = pixelArt },
+
+    { "characters/duck1.png", "characters.duck1", onLoad = setFilter, "nearest" },
   }
