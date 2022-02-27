806946e5489116758cce644d46645a1a0e171fb5
EngineerSmith <pasmith.bus@gmail.com>
2022-02-27 17:30:01 +0000

    Added makePixelAnimation callback function for assets

--- assets/defineAssets.lua
+++ assets/defineAssets.lua
@@ -1,16 +1,27 @@
+local anim8 = require("libs.anim8")
+
 local pixelArt = function(image)
   image:setFilter("nearest", "nearest")
 end
 
+local makePixelAnimation = function(image, frameCount, frameWidth, frameHeight, frameTime)
+  pixelArt(image)
+  local grid = anim8.newGrid(frameWidth, frameHeight, image:getDimensions())
+  return anim8.newAnimation(grid('1-'..frameCount, 1), frameTime)
+end
+
 return { 
+    -- UI
     { "UI/logoES.png", "logo.ES", onLoad = pixelArt },
     
+    -- Tiles
     { "tiles/test1.png", "tiles.test1", onLoad = pixelArt },
     { "tiles/test2.png", "tiles.test2", onLoad = pixelArt },
     { "tiles/test3.png", "tiles.test3", onLoad = pixelArt },
     { "tiles/grass.png", "tiles.grass", onLoad = pixelArt },
     { "tiles/stone.png", "tiles.stone", onLoad = pixelArt },
-    { "tiles/water.png", "tiles.water", onLoad = pixelArt },
-
+    { "tiles/water.png", "tiles.water", onLoad = makePixelAnimation, 8, 32, 32, 0.2},
+    
+    -- Characters
     { "characters/duck1.png", "characters.duck1", onLoad = pixelArt },
   }
