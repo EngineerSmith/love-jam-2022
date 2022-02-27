974ece4f66e681df01a7010df13e569e4251ed97
rhy <81539300+rhynomatt@users.noreply.github.com>
2022-02-27 13:55:59 -0300

    Add grass/stone/water tile textures
    The water animation should play at 80ms per frame (12fps) but it'll
    probably look ok at anything near that

--- assets/defineAssets.lua
+++ assets/defineAssets.lua
@@ -5,9 +5,12 @@ end
 return { 
     { "UI/logoES.png", "logo.ES", onLoad = pixelArt },
     
-	  { "tiles/test1.png", "tiles.test1", onLoad = pixelArt },
+    { "tiles/test1.png", "tiles.test1", onLoad = pixelArt },
     { "tiles/test2.png", "tiles.test2", onLoad = pixelArt },
     { "tiles/test3.png", "tiles.test3", onLoad = pixelArt },
+    { "tiles/grass.png", "tiles.grass", onLoad = pixelArt },
+    { "tiles/stone.png", "tiles.stone", onLoad = pixelArt },
+    { "tiles/water.png", "tiles.water", onLoad = pixelArt },
 
-    { "characters/duck1.png", "characters.duck1", onLoad = pixelArt,},
+    { "characters/duck1.png", "characters.duck1", onLoad = pixelArt },
   }
