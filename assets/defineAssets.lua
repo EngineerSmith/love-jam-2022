2f2ffe3becfcaacc7a46b7d61130b63b0fc6e2e8
rhy <81539300+rhynomatt@users.noreply.github.com>
2022-03-01 17:59:44 -0300

    Add UI placement arrow

--- assets/defineAssets.lua
+++ assets/defineAssets.lua
@@ -22,6 +22,7 @@ return {
     { "UI/wheel_SW.png", "ui.wheel.se", onLoad = pixelArt },
     { "UI/wheel_SE.png", "ui.wheel.sw", onLoad = pixelArt },
     { "UI/coin.png", "ui.coin", onLoad = pixelArt },
+    { "UI/arrow.png", "ui.arrow", onLoad = makePixelAnimation, 15, 32, 64, 0.08 },
     
     -- Tiles
     { "tiles/test1.png", "tiles.test1", onLoad = pixelArt },
